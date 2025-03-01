61cc7fd1c731e03339154f9c297a6340791a78ea
𒅒𝗔𝗧𝗢𝗠𝗜𝗖ᵗᵉᶜʰ✰ཽ <akuebenjamin01@gmail.com>
2025-03-01 12:20:24 +0000

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'client/dist', // Ceci force Vite à générer le build dans ce dossier
+  },
 });
