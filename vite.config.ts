bf0119d044da5bf729b0252a6c9e3d77c8b3096c
𒅒𝗔𝗧𝗢𝗠𝗜𝗖ᵗᵉᶜʰ✰ཽ <akuebenjamin01@gmail.com>
2025-03-01 12:41:48 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default async () => {
     },
     root: path.resolve(__dirname, "client"),
     build: {
-      outDir: path.resolve(__dirname, "client", "dist"), // Utilisation de "client/dist" pour la sortie
+      outDir: path.resolve(__dirname, "dist/public"),
       emptyOutDir: true,
     },
   });
